334f84cf840c81cbb086c9117ccc1ba2c6268361
Jammie506 <mccarthyjamie506@gmail.com>
2024-05-29 12:45:06 +0100

    Create function for printing both boards

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Project Imports
+import random
+
+#Creating Generic Board Layout
+def create_board(board):
+    print(" A  B  C  D  E  F  G  H")
+    print("________________________")
+    row_num = 1
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num += 1
